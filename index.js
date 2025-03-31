66f52a1be9e3706354d58fb3ece48c29c10f4dbf
harshitmudgal <44114120+harshitmudgal@users.noreply.github.com>
2025-03-31 13:39:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello, You are  in AWS  Cloud.'); //write a response to the client
+  res.write('Hi, You are  in AWS  Cloud.'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
